d76cf64fee1b083841c441a281eaf2c851d66e4c
Jessica Gillan <gillan.jessica@gmail.com>
2017-08-19 12:43:12 -0600

    display list of cards

---
+++ app/models/card.rb
@@ -0,0 +1,16 @@
+class Card
+  def self.get_all
+    return [
+      {
+        name: "Capital One Venture Card",
+        earnings: "300 miles",
+        cash_value: "$150.00"
+      },
+      {
+        name: "Capital One Cash Card",
+        earnings: "300 dollars",
+        cash_value: "$100.00"
+      }
+    ]
+  end
+end
